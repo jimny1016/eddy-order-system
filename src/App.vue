a3c679d505edbbb6b229e4115178b7abad3a287d
jimny1016 <jimny1014@gmail.com>
2023-06-24 16:01:47 +0800

    Clean App.vue.

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,9 @@
 <template>
-
-  <div>
-    <Navbar tableNum="3" title="Welcom to xxx restaurant"/>
-  </div><br><br>
-
-<!--
-
-    <Slider/>
- --> 
-
-  <div>
-    <CategoryTabs class="w-full lg:w-10/12 mx-auto mb-16" :tabList="tabList">
-      <div v-for="(tab, index) in tabList" :key="index" >
-        <slot  tabindex=${index}>{{ tab }}</slot>
-      </div>
-    </CategoryTabs>
-  </div>
-<Menupage/>
-
+  <MyHome></MyHome>
 </template>
 
 <script>
-//import HelloWorld from './components/HelloWorld.vue'
-//import Slider from './components/Carousel.vue';
-import Navbar from './components/NavigationHeaderBar.vue';
-import CategoryTabs from './components/CategoryTabs.vue';
-import Menupage from './components/Menu.vue';
+import MyHome from './components/MyHome.vue';
 //string ddd=
  // {
  //   /"Name/": /"Jimmy/"
@@ -39,13 +17,7 @@ import Menupage from './components/Menu.vue';
 export default {
   name: 'App',
   components: {
-  //  HelloWorld, Slider,
-  Navbar,CategoryTabs,Menupage,
-  },
-  data() {
-      return {
-      tabList: ["Recommendation", "Special Combo", "a la carte - Rice", "a la carte - Noodle","a la carte - Side Dish","a la carte - Soup","a la carte - Beverage"],
-      };
+    MyHome
   },
 };
 
---
+++ src/components/MyHome.vue
@@ -0,0 +1,41 @@
+<template>
+  <Slider />      
+  <div>
+    <Navbar tableNum="3" title="Welcom to xxx restaurant"/>
+  </div>
+  <br>
+  <br>
+  <div>
+    <CategoryTabs class="w-full lg:w-10/12 mx-auto mb-16" :tabList="tabList">
+      <div v-for="(tab, index) in tabList" :key="index" >
+        <slot  tabindex=${index}>{{ tab }}</slot>
+      </div>
+    </CategoryTabs>
+  </div>
+  <Menupage/>
+</template>
+  
+<script>
+  import Slider from './Carousel.vue';
+  import Navbar from './NavigationHeaderBar.vue';
+  import CategoryTabs from './CategoryTabs.vue';
+  import Menupage from './Menu.vue';
+
+  export default {
+    name: "my-home",
+    components: {
+      Slider,
+      Navbar,
+      CategoryTabs,
+      Menupage,
+    },
+    data() {
+      return {
+        tabList: ["Recommendation", "Special Combo", "a la carte - Rice", "a la carte - Noodle","a la carte - Side Dish","a la carte - Soup","a la carte - Beverage"],
+      };
+    },
+  };
+</script>
+
+<style scoped>
+</style>
