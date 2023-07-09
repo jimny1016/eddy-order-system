68c4dce9caa89e1e93deb9f1a288a88452ab6f64
jimny1016 <jimny1014@gmail.com>
2023-07-09 15:32:44 +0800

    Add radio bottun.

--- src/components/DishesDetail.vue
+++ src/components/DishesDetail.vue
@@ -38,8 +38,29 @@
                             </div>
                         </div>
                     </div>
-                    <div v-for="(optionVaule, optionVauleIndex) in option.OptionVaules" :key="'optionVaule-' + optionVauleIndex" @click="optionVaule.BeChoise = !optionVaule.BeChoise" class="flex items-center space-x-2 my-2 cursor-pointer">
+                    <div v-for="(optionVaule, optionVauleIndex) in option.OptionVaules" :key="'optionVauleChecobox-' + optionVauleIndex" @click="optionVaule.BeChoise = !optionVaule.BeChoise" class="flex items-center space-x-2 my-2 cursor-pointer">
                         <input type="checkbox" @click.stop v-model="optionVaule.BeChoise" class="w-5 h-5 text-blue-500 rounded" /> 
+                        <label class="text-lg">{{ optionVaule.ValueName }}</label>
+                    </div>
+                </div>
+                <div v-if="option.Type=='2'">
+                    <div class="flex justify-between">
+                        <div>
+                            <div class="text-xl font-bold">
+                                {{ option.Name }}
+                            </div>
+                            <div class=" text-gray-500">
+                                {{ option.Memo }}
+                            </div>
+                        </div>
+                        <div>
+                            <div class="bg-blue-500 rounded-full text-white py-0.5 px-2">
+                                必填                        
+                            </div>
+                        </div>
+                    </div>
+                    <div v-for="(optionVaule, optionVauleIndex) in option.OptionVaules" :key="'optionVauleRadio-' + optionVauleIndex" @click="handleRadioChange(optionIndex, optionVauleIndex)" class="flex items-center space-x-2 my-2 cursor-pointer">
+                        <input type="radio" :name="'radio-' + option.Key" @click.stop :checked="optionVaule.BeChoise" class="w-5 h-5 text-blue-500 rounded" /> 
                         <label class="text-lg">{{ optionVaule.ValueName }}</label>
                     </div>
                 </div>
@@ -79,8 +100,13 @@
             },
         },       
         methods:{
-            sendValue(){
-                // 可以在这里执行发送值的逻辑
+            handleRadioChange(optionIndex, optinValueIndex) {
+                this.disheOptions[optionIndex].OptionVaules.forEach((optionVaule, index) => {
+                    if(index == optinValueIndex)
+                        optionVaule.BeChoise = true;
+                    else
+                        optionVaule.BeChoise = false;
+                });
             },
             backToMenu(){
                 this.$emit('backToMenu');
