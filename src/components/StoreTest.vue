dd7f321e3b3abdbdbdba1b5c4f2e5b79e0c93653
jimny1016 <jimny1014@gmail.com>
2023-07-10 09:51:08 +0800

    Fix rule.

--- src/components/StoreTest.vue
+++ src/components/StoreTest.vue
@@ -23,9 +23,110 @@
         },
         setup() {
             const store = useStore();
-            // var ddd = { name: "ddd", id: 1 };
-            // var one = 1;
-            // store.dispatch('addToCart', {product: ddd, quantity: one });
+            var ddd = {
+                        title: "招牌蛋炒飯",
+                        DishKey:"recommend_1",
+                        unit: "一份",
+                        imgPath: "../src/image",
+                        price: 90,
+                        Options: [
+                        {
+                            Type:2,//1=check box, 2=radio button, 3=textBox, 4=count
+                            Requirement:true,//0=option, 1=required
+                            Key:1,//1=辣度選擇,2=加購,3=備註
+                            Name:"辣度選擇",
+                            Memo:"Please Select 1 option",
+                            OptionVaules:[{
+                            ValueName:"不辣",
+                            ValueKey:"1",
+                            Price:0,
+                            BeChoise:false,
+                            Content:""
+                            },{
+                            ValueName:"小辣",
+                            ValueKey:"2",
+                            Price:0,
+                            BeChoise: false,
+                            Content:""
+                            },{
+                            ValueName:"中辣",
+                            ValueKey:"3",
+                            Price:0,
+                            BeChoise: false,
+                            Content:""
+                            },{
+                            ValueName:"大辣",
+                            ValueKey:"4",
+                            Price:0,
+                            BeChoise: false,
+                            Content:""
+                            }]
+                        },
+                        {
+                            Type:1,
+                            Requirement:false,
+                            Key:2,
+                            Name:"加購",
+                            Memo:"Recommendation",
+                            OptionVaules:[{
+                            ValueName:"Coca Cola",
+                            ValueKey:"1",
+                            Price:30,
+                            BeChoise:false,
+                            Content:""
+                            },{
+                            ValueName:"Apple Sidra",
+                            ValueKey:"2",
+                            Price:40,
+                            BeChoise: false,
+                            Content:""
+                            },{
+                            ValueName:"Pepsi",
+                            ValueKey:"3",
+                            Price:50,
+                            BeChoise: false,
+                            Content:""
+                            },{
+                            ValueName:"Sprite",
+                            ValueKey:"4",
+                            Price:60,
+                            BeChoise: false,
+                            Content:""
+                            }]
+                        },
+                        {
+                            Type:4,//1=check box, 2=radio button, 3=textBox, 4=count
+                            Requirement:false,//0=option, 1=required
+                            Key:3,//1=辣度選擇,2=加購,3=備註
+                            Name:"加購",
+                            Memo:"若超過10人享用,請加選用餐人數",
+                            OptionVaules:[{
+                            ValueName:"增加人數",
+                            ValueKey:"1",
+                            Price:200,
+                            BeChoise:false,
+                            Content:"0"
+                            }]
+                        },
+                        {
+                            Type:3,//1=check box, 2=radio button, 3=textBox, 4=count
+                            Requirement:false,//0=option, 1=required
+                            Key:4,//1=辣度選擇,2=加購,3=備註
+                            Name:"備註",
+                            Memo:"如有任何需求或打包服務請備註",
+                            OptionVaules:[{
+                            ValueName:"備註",
+                            ValueKey:"1",
+                            Price:0,
+                            BeChoise:false,
+                            Content:""
+                            }]
+                        },
+                        ]
+                    };
+            var one = 1;
+            store.dispatch('addToCart', {product: ddd, quantity: one });
+            store.dispatch('addToCart', {product: ddd, quantity: one });
 
             const cart = computed(() => store.getters.cart);
             console.log(cart);
--- src/store/index.js
+++ src/store/index.js
@@ -6,26 +6,26 @@ export default createStore({
   },
   mutations: {
     // 新增產品到購物車
-    ADD_TO_CART(state, { product: dish, quantity }) {
+    ADD_TO_CART(state, { product, quantity }) {
       try {
         if(state.cart && state.cart.length > 0)
         {
-          let dishesInCart = state.cart.find(item => {
-            return item.DishKey === dish.DishKey;
+          let dishesInCart = state.cart.filter(item => {
+            return item.product.DishKey === product.DishKey;
           });
       
-          if (dishesInCart) {
+          if (dishesInCart) { 
             for (let dishesInCartIndex = 0; dishesInCartIndex < dishesInCart.length; dishesInCartIndex++) {
               let isLivedInShoppingCart = true;
-              let dishInCart = dishesInCart[dishesInCartIndex];
+              let dishInCart = dishesInCart[dishesInCartIndex].product;
               if(dishInCart.Options)
-              {                
+              {
                 for (let optionIndex = 0; optionIndex < dishInCart.Options.length; optionIndex++) {
                   if(!isLivedInShoppingCart) {
                     break;
                   }
                   let option = dishInCart.Options[optionIndex];
-                  let targetOption = dish.Options[optionIndex];
+                  let targetOption = product.Options[optionIndex];
 
                   for (let optionVaulesIndex = 0; optionVaulesIndex < option.OptionVaules.length; optionVaulesIndex++) {
                     if(!isLivedInShoppingCart) {
@@ -49,16 +49,16 @@ export default createStore({
                 }
               }
 
-              if(isLivedInShoppingCart) {                
-                dishesInCart.quantity += quantity;
+              if(isLivedInShoppingCart) {
+                dishesInCart[dishesInCartIndex].quantity += quantity;
                 return;
-              }              
+              }
             }
           }
         }
 
         state.cart.push({
-            product: dish,
+            product,
             quantity
         });
       } catch (error) {
