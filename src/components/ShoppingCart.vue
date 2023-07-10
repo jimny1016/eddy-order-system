61fa477e9b499800e4dd3d9407b890c585062759
jimny1016 <jimny1014@gmail.com>
2023-07-10 22:37:07 +0800

    Add shopping cart ver 0.1

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,16 +1,59 @@
 <template>
     <div>
-        123
+        <div class="flex py-4">
+            <div @click="backToMenu()" class="cursor-pointer">
+                <MyImage imagePath="/image/icon/previous-blue.png" firstLayerClass="mr-2 z-10 !w-auto !h-auto" secondLayerClass="!w-auto !h-auto" imageClass="!w-10 !h-10" />
+            </div>
+            <div class="text-2xl font-semibold self-center">
+                購物車
+            </div>
+        </div>
+        <div>
+            <div class="text-lg font-semibold my-2">
+                您的餐點
+            </div>
+            <div>
+                <div v-for="(dish, dishIndex) in cart" :key="'dish-' + dishIndex" class="mb-8 w-full grid grid-cols-7">
+                    <div class="flex">
+                        <MyImage imagePath="/image/icon/minus.png" @click="()=>{  }"  firstLayerClass="!w-auto !h-auto cursor-pointer" secondLayerClass="!w-auto !h-auto" imageClass="!w-8 !h-8" />
+                        <div class="mx-2 self-center text-center text-lg">{{ dish.quantity }}</div>
+                        <MyImage imagePath="/image/icon/plus.png" @click="()=>{  }"  firstLayerClass="!w-auto !h-auto cursor-pointer" secondLayerClass="!w-auto !h-auto" imageClass="!w-8 !h-8" />
+                    </div>
+                    <div class="col-span-5 self-center text-xl font-bold">
+                        {{ dish.product.title }}
+                    </div>
+                    <div class="self-center text-right text-lg ">
+                        $ {{ dish.product.price }}
+                    </div>
+                    <div></div>
+                    <div class="col-span-5">123</div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+        {{ cart }}
     </div>
 </template>
 
 <script>
+    import MyImage from './tools/MyImage.vue';
     export default {
+        components: {
+            MyImage,
+        },
         data() {
             return {
             };
         },
+        computed:{
+            cart() {
+                return this.$store.getters.cart;
+            }
+        },
         methods: {
+            backToMenu(){
+                this.$store.dispatch('updatePageState', {pageState: 0 });
+            },
         }
     };
 </script>
