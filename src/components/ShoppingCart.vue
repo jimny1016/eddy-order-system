cdbd9e24d66e3645455a4f443543115b777a3bfa
jimny1016 <jimny1014@gmail.com>
2023-07-11 19:04:26 +0800

    Add money function block.

--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -30,6 +30,30 @@
                     <div></div>
                 </div>
             </div>
+            <div class="flex justify-between mb-2">
+                <div class="text-xl font-semibold">
+                    小計
+                </div>
+                <div class="text-xl">
+                    $ {{ getLittlePrice(cart) }}
+                </div>
+            </div>
+            <div class="flex justify-between mb-8">
+                <div class="text-xl font-semibold">
+                    服務費
+                </div>
+                <div class="text-xl">
+                    $ {{ getServicePrice(cart) }}
+                </div>
+            </div>
+            <div class="flex justify-between mb-8">
+                <div class="text-2xl font-bold">
+                    總金額
+                </div>
+                <div class="text-2xl font-bold">
+                    $ {{ getTotalPrice(cart) }}
+                </div>
+            </div>
         </div>
         {{ cart }}
     </div>
@@ -83,7 +107,7 @@
             getDishPrice(dish){
                 var product = dish.product;
                 let result = product.price;
-                product.Options.forEach((option) => {                    
+                product.Options.forEach((option) => {
                     option.OptionVaules.forEach((optionVaule) => {
                         switch(option.Type) {
                         case 1:
@@ -104,6 +128,19 @@
                 });
 
                 return result * dish.quantity;
+            },
+            getLittlePrice(cart){
+                let result = 0;
+                cart.forEach((dish) => {
+                    result += this.getDishPrice(dish);
+                });
+                return result;
+            },
+            getServicePrice(cart){
+                return this.getLittlePrice(cart) * 0.1;
+            },
+            getTotalPrice(cart){
+                return this.getLittlePrice(cart) + this.getServicePrice(cart);
             }
         }
     };
