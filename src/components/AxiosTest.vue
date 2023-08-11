5259e42c84528ee59633968235a98d02ac9585c0
jimny1016 <jimny1014@gmail.com>
2023-08-11 20:57:41 +0800

    Revert "Menu data from backend."
    This reverts commit bcfe6bddfa8ce0ef5602bed812c98bb70826db55.

--- src/components/AxiosTest.vue
+++ src/components/AxiosTest.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- <button @click="makePostRequest">發送 POST 請求</button> -->
     <button @click="makeGetRequest">發送 GET 請求</button>
   </div>
 </template>
@@ -9,6 +10,16 @@ import axios from 'axios';
 
 export default {
   methods: {
+    makePostRequest() {
+      const data = { name: 'John', age: 25 };
+      axios.post('http://ebby123.com.tw/api/{controller}/{action}', data)
+        .then(response => {
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
     makeGetRequest() {
       axios.get('http://localhost:5126/weatherforecast/GetWeatherForecast2')
         .then(response => {
